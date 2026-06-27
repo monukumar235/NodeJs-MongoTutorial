d568de6c0746e26d14bfe75699a414de610ad0c5
monu kumar <69889172+mkagarwal235@users.noreply.github.com>
2026-06-27 11:55:31 +0530

    added new changes

--- app.js
+++ app.js
@@ -11,26 +11,33 @@ app.use(express.json());
 const url = "mongodb://localhost:27017";
 const dbName = "StudentDb";
 
+app.set("view engine",'pug');
+
 const client = new MongoClient(url);
 
-app.set('view engine','pug');
+let db;
 
-app.get("/data",async (req,res)=>{
-    try{
-        await client.connect();
-        console.log("Connected to server");
+async function connectTODb(){
+    await client.connect();
+    console.log("Connected to Server SuccessFully....");
+    db = client.db(dbName);
+}
 
-        const db = client.db(dbName);
-        const collection = db.collection("students");
-        const data = await collection.find({age :{$gte:25}}).toArray();
-        console.log(data);
-        res.render("index",{students : data});
-        // res.json(data);
-    }catch(error){
-        console.log("An Error Occured" + error);
-        res.status(500).send({error : "An Error Occured."});
-    }
+app.get("/student", async (req,res)=>{
+    connectTODb();
+    const data = await db.collection('students').find({}).toArray();
+    console.log(data);
+    res.json(data);
 });
+
+// Rendering data in pug
+app.get("/data", async (req,res)=>{
+    connectTODb();
+    const data = await db.collection('students').find({}).toArray();
+    console.log(data);
+    res.status(200).render("index",{students:data});
+});
+
 
 app.listen(port,()=>{
     console.log(`Server Started at port http://localhost:${port}`);
