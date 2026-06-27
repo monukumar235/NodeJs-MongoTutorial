e6e0815dda5342dc6ff1cfb53b4f48a412881935
monu kumar <69889172+mkagarwal235@users.noreply.github.com>
2026-06-27 13:04:54 +0530

    added new changes

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ import path from 'path';
 const app = express();
 const port = 80;
 
+// middleware in express
 app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static('public'));
 
 const url = "mongodb://localhost:27017";
 const dbName = "StudentDb";
@@ -24,7 +27,6 @@ async function connectTODb(){
 }
 
 app.get("/student", async (req,res)=>{
-    connectTODb();
     const data = await db.collection('students').find({}).toArray();
     console.log(data);
     res.json(data);
@@ -32,13 +34,20 @@ app.get("/student", async (req,res)=>{
 
 // Rendering data in pug
 app.get("/data", async (req,res)=>{
-    connectTODb();
     const data = await db.collection('students').find({}).toArray();
     console.log(data);
     res.status(200).render("index",{students:data});
 });
 
+app.post("/add-student",async (req,res)=>{
+    console.log(req.body);
+    const staudentData = req.body;
+    await db.collection("students").insertOne(staudentData);
+    res.send(`<h3>Student Added Successfully</h3><a href="/form.html">GO Back</a>`)
+})
 
-app.listen(port,()=>{
+
+app.listen(port,async ()=>{
+     await connectTODb();
     console.log(`Server Started at port http://localhost:${port}`);
 });
